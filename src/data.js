c584133bdef4aafe3f5b4cb13a0f21e520e4898b
shuler7 <vitalisobolevggg@gmail.com>
2025-09-25 14:20:38 +0300

    Change API routes for deploying

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 export const API_ROUTES = {
-    'authenticate': 'http://localhost:8000/authenticate_user',
-    'refresh': 'http://localhost:8000/refresh_user',
-    'register': 'http://localhost:8000/register',
-    'login': 'http://localhost:8000/login',
-    'signout': 'http://localhost:8000/signout',
-    'create_note': 'http://localhost:8000/create_new_note',
-    'get_notes': 'http://localhost:8000/get_notes',
-    'change_note_status': 'http://localhost:8000/change_note_status'
+    'authenticate': 'https://todoapp-api-hy80.onrender.com/authenticate_user',
+    'refresh': 'https://todoapp-api-hy80.onrender.com/refresh_user',
+    'register': 'https://todoapp-api-hy80.onrender.com/register',
+    'login': 'https://todoapp-api-hy80.onrender.com/login',
+    'signout': 'https://todoapp-api-hy80.onrender.com/signout',
+    'create_note': 'https://todoapp-api-hy80.onrender.com/create_new_note',
+    'get_notes': 'https://todoapp-api-hy80.onrender.com/get_notes',
+    'change_note_status': 'https://todoapp-api-hy80.onrender.com/change_note_status'
 };
 
 export const SECTION_NAME_BY_KEY = {
