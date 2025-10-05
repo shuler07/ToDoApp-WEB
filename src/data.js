86e18f6fe43fb1635c0f31bd6dc8f673c8e082b5
Vitaliy <viasobolev@mai.education>
2025-10-05 18:37:41 +0300

    Merge api routes for deploy and debug in single file

--- src/data.js
+++ src/data.js
@@ -1,13 +1,25 @@
-export const API_ROUTES = {
-    'authenticate': 'http://localhost:8000/authenticate_user',
-    'refresh': 'http://localhost:8000/refresh_user',
-    'register': 'http://localhost:8000/register',
-    'login': 'http://localhost:8000/login',
-    'signout': 'http://localhost:8000/signout',
-    'create_note': 'http://localhost:8000/create_new_note',
-    'get_notes': 'http://localhost:8000/get_notes',
-    'update_note': 'http://localhost:8000/update_note',
-    'delete_note': 'http://localhost:8000/delete_note'
+const DEBUG = true;
+
+export const API_ROUTES = DEBUG ? {
+    authenticate: 'http://localhost:8000/authenticate_user',
+    refresh: 'http://localhost:8000/refresh_user',
+    register: 'http://localhost:8000/register',
+    login: 'http://localhost:8000/login',
+    signout: 'http://localhost:8000/signout',
+    create_note: 'http://localhost:8000/create_new_note',
+    get_notes: 'http://localhost:8000/get_notes',
+    update_note: 'http://localhost:8000/update_note',
+    delete_note: 'http://localhost:8000/delete_note'
+} : {
+    authenticate: 'https://todoapp-api-hy80.onrender.com/authenticate_user',
+    refresh: 'https://todoapp-api-hy80.onrender.com/refresh_user',
+    register: 'https://todoapp-api-hy80.onrender.com/register',
+    login: 'https://todoapp-api-hy80.onrender.com/login',
+    signout: 'https://todoapp-api-hy80.onrender.com/signout',
+    create_note: 'https://todoapp-api-hy80.onrender.com/create_new_note',
+    get_notes: 'https://todoapp-api-hy80.onrender.com/get_notes',
+    update_note: 'https://todoapp-api-hy80.onrender.com/update_note',
+    delete_note: 'https://todoapp-api-hy80.onrender.com/delete_note'
 };
 
 export const SECTION_NAME_BY_KEY = {
